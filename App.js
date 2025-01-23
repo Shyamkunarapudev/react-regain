f671b96da6c889a5a07ff76d4fabd81607dac270
shyamkunarapudev <shyamkumar14318@gmail.com>
2025-01-23 07:22:41 +0000

    episode-01

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1", {id: "heading"}, "Namaste React from React CDN");
+    
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
